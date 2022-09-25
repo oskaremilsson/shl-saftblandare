80abcf898204c83c10a0a3b28b748a4a51e891f2
Oskar Emilsson <oskaremilsson@gmail.com>
2022-09-25 13:09:44 +0200

    Added bouncetime to button

--- button.py
+++ button.py
@@ -3,13 +3,13 @@ import RPi.GPIO as GPIO
 
 def button_callback(channel):
   print("Button was pushed!")
-  cmd = "uhubctl -l 1-1 -p 2 -a toggle"
-  os.system(cmd)
+  os.system("uhubctl -l 1-1 -p 2 -a toggle")
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
+
+GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback, bouncetime=1000)
 
 message = input("Press enter to quit\n")
 GPIO.cleanup()
